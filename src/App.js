7674edf59502ecce18c4b8302fab7c03a057080b
Faithkebenei <faithkebs16@gmail.com>
2023-06-12 23:25:34 +0300

    Enabled searching movies

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
                 <img
                 src={SearchIcon}
                 alt="search"
-                onClick={() => {}}/>
+                onClick={() => searchMovies(searchTerm)}/>
             </div>
 
             {
