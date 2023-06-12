71d543507fbc0941fb3cfcb3961c489f7aa00aff
Faithkebenei <faithkebs16@gmail.com>
2023-06-12 22:45:30 +0300

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+// e0bf230d
+const API_URL = "http://omdbapi.com?apikey=e0bf230d";
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(()=> {
+        searchMovies('spiderman');
+    }, [])
+    
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                placeholder="Search for movies"
+                value="Superman"
+                onChange={() => {}}></input>
+                <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {}}/>
+            </div>
+        </div>
     );
 }
 
